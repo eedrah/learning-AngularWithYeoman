e97f629b9bf5e17d75d6169e91a7926c6f66f40e
Eedrah <eedrah@users.noreply.github.com>
2015-05-15 08:22:12 +1200

    scaffold with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name twoohfoureightApp
+ * @description
+ * # twoohfoureightApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('twoohfoureightApp', [
+    'ngCookies'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twoohfoureightApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the twoohfoureightApp
+ */
+angular.module('twoohfoureightApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
