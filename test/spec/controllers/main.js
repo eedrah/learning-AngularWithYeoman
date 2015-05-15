1041d5b62759b4d3e118abc1100ed8307768ee55
Eedrah <eedrah@users.noreply.github.com>
2015-05-16 08:37:57 +1200

    start writing tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,22 +1,33 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Game module', function() {
+    describe('Game manager', function() {
+        beforeEach(module('Game'));
 
-  // load the controller's module
-  beforeEach(module('twoohfoureightApp'));
-
-  var MainCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+        var gameManager;
+        beforeEach(inject(function(GameManager) {
+            gameManager = GameManager;
+        }));
     });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
+
+//describe('Controller: MainCtrl', function () {
+
+//  // load the controller's module
+//  beforeEach(module('twoohfoureightApp'));
+
+//  var MainCtrl,
+//    scope;
+
+//  // Initialize the controller and a mock scope
+//  beforeEach(inject(function ($controller, $rootScope) {
+//    scope = $rootScope.$new();
+//    MainCtrl = $controller('MainCtrl', {
+//      $scope: scope
+//    });
+//  }));
+
+//  it('should attach a list of awesomeThings to the scope', function () {
+//    expect(scope.awesomeThings.length).toBe(3);
+//  });
+//});
