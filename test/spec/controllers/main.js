b5a10bafbefb337c7def4b5aaa537a9da4cc124c
Eedrah <eedrah@users.noreply.github.com>
2015-05-19 15:15:10 +1200

    write gameManager tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,6 +1,16 @@
 'use strict';
 
-describe('Game module', function() {
+describe('Game module', function () {
+    var _gridService;
+    beforeEach(module(function($provide) {
+        _gridService = {
+            anyCellsAvailable: angular.noop,
+            titleMatchesAvailable: angular.noop
+        };
+
+        $provide.value('GridService', _gridService); 
+    }));
+
     describe('Game manager', function() {
         beforeEach(module('Game'));
 
@@ -8,6 +18,25 @@ describe('Game module', function() {
         beforeEach(inject(function(GameManager) {
             gameManager = GameManager;
         }));
+
+        describe('.movesAvailable', function() {
+            it('should report true if there are cells available', function() {
+                spyOn(_gridService, 'anyCellsAvailable').andReturn(true);
+                expect(gameManager.movesAvailable()).toBeTruthy();
+            });
+
+            it('should report true if there are matches available', function() {
+                spyOn(_gridService, 'anyCellsAvailable').andReturn(false);
+                spyOn(_gridService, 'tileMatchesAvailable').andReturn(true);
+                expect(gameManager.movesAvailable()).toBeTruthy();
+            });
+
+            it('should report false if there are no cells nor matches available', function() {
+                spyOn(_gridService, 'anyCellsAvailable').andReturn(false);
+                spyOn(_gridService, 'tileMatchesAvailable').andReturn(false);
+                expect(gameManager.movesAvailable()).toBeFalsy();
+            });
+        });
     });
 });
 
